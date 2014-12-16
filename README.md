ef44f11d35de320436dbeb1b9c250b1a2bda6e69
Robin Kearney <robin@kearney.co.uk>
2014-12-16 22:29:43 +0000

    MQTT subscriber script

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Maplins switch
 
+Some of the code I've hacked together while playing with the [Maplin remote](http://www.maplin.co.uk/p/remote-controlled-mains-sockets-5-pack-n38hn) 
+switches and my [Raspberry Pi](http://www.raspberrypi.org/).
+
+## switch.py
+
 This is a simple wrapper script to [Raspberry Strogonanoff](https://github.com/dmcg/raspberry-strogonanoff) it allows me to (for example) say things like:
 
 ```
@@ -8,4 +13,24 @@ This is a simple wrapper script to [Raspberry Strogonanoff](https://github.com/d
 
 Which is nicer than having to remember the channel and button numbers. It also runs the sudo for you.
 
+## maplin-mqtt.py
 
+Another simple wrapper which connects to a mqtt broker and subscribes to a specific topic. Then it sits there looking for messages of the format:
+
+``
+`{"action": "off", "switch": "spareroom"}'
+```
+
+This is intended to make it easier to communicate with the Pi from remote machines. To test this you can use mosquitto_sub:
+
+```
+% mosquitto_pub -h trin -t foo/bar -m '{"action": "off", "switch": "spareroom"}'
+```
+
+Which should result in something like the following being printed on stdout of the maplin-mqtt.py script:
+
+```
+2014-12-16 22:22:10.149430 action=off channel=3 button=1
+```
+
+And you never know, channel 3, button 1 ***might*** have just turned off :)
---
+++ maplin-mqtt.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python -u
+# -u to unbuffer stdout, plays nicer with supervisor
+
+import sys
+import json
+import datetime
+import paho.mqtt.client as paho
+from subprocess import Popen, PIPE
+
+""" hostname and topic of MQTT """
+hostname = 'trin'
+topic = 'foo/bar'
+
+""" Master lookup table of friendly names to channel/button"""
+switches = { 
+            'bookshelf': { 'channel' : 4, 'button'  : 1 },
+            'tank':      { 'channel' : 4, 'button'  : 2 },
+            'sofa':      { 'channel' : 4, 'button'  : 3 },
+            'outside':   { 'channel' : 4, 'button'  : 4 },
+            'spareroom': { 'channel' : 3, 'button'  : 1 },
+            'all' : True
+           }
+
+
+def runcmd(channel, button, action):
+
+    """Does the grunt work of calling strogonanoff_sender.py
+
+    Builds an array of strings to pass to Popen. Currently needs NOPASSWD sudo 
+    access to interact with /dev/mem.
+    """
+
+    # TODO: Figure out if a user can be given privs to access /dev/mem
+
+    cmd = ["/usr/bin/sudo", "/usr/local/bin/strogonanoff_sender.py", "-c", str(channel), "-b", str(button), action]
+    try:
+        p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    except Exception, e:
+        print("error:  %s" % e.strerror)
+        print("failed to run command: \"%s\"" % ' '.join(command))
+    
+
+
+def on_message(client, userdata, message):
+    """Called whenever a message is received. 
+
+    Tries to parse the message as valid json and if successful will
+    try to find the switch mentioned in the json in the global lookup
+    table 'switches' (defined at the top). If the switch is found it
+    will call runcmd to actually do the work of turning the switch 
+    on or off.
+    """
+
+    timestamp = datetime.datetime.now()
+
+    try:
+        data = json.loads(message.payload)
+    except:
+        print "%s failed to parse json. message=%s" % (timestamp, message.payload)
+        return False
+
+    if data['switch'] not in switches:
+        print "%s switch (%s) not found" % (timestamp, data['switch'])
+        return False
+
+    action  = data['action']
+    channel = switches[data['switch']]['channel']
+    button  = switches[data['switch']]['button']
+    print "%s action=%s channel=%s button=%s" % (timestamp, action, channel, button)
+
+    runcmd(channel, button, action)
+
+
+if __name__ == "__main__":
+
+    mqttc = paho.Client()
+    mqttc.on_message = on_message
+    mqttc.connect(hostname)
+    mqttc.subscribe(topic)
+
+    while mqttc.loop() == 0:
+        pass
