8a88c0499695a556e690e08280f2a45faa23dbd6
Robin Kearney <robin@kearney.co.uk>
2016-01-10 19:28:49 +0000

    Bunch of small changes, and now configures itself.
    Expects to find a json payload on MQTT_TOPIC/switches which it uses
    to configure itself. Also watches for live changes to its config.

--- maplin-mqtt.py
+++ maplin-mqtt.py
@@ -11,46 +11,39 @@ from subprocess import Popen, PIPE
 
 """ hostname and topic of MQTT """
 hostname = os.getenv('MQTT_HOST')
-topic = os.getenv('MQTT_TOPIC')
+topic_base = os.getenv('MQTT_TOPIC')
+topic_wildcard = topic_base + "#"
+topic_toggle = topic_base + 'toggle'
+topic_switches = topic_base + 'switches'
 """ Optional username and password for MQTT """
 username = os.getenv('MQTT_USERNAME', None)
 password = os.getenv('MQTT_PASSWORD', None)
 
-""" Master lookup table of friendly names to channel/button"""
-switches = { 
-            'bookshelf': { 'channel' : 4, 'button'  : 1 },
-            'tank':      { 'channel' : 4, 'button'  : 2 },
-            'sofa':      { 'channel' : 4, 'button'  : 3 },
-            'outside':   { 'channel' : 4, 'button'  : 4 },
-            'spareroom': { 'channel' : 3, 'button'  : 1 },
-            'all' : True
-           }
-
 
 def runcmd(channel, button, action):
-
     """Does the grunt work of calling strogonanoff_sender.py
 
     Builds an array of strings to pass to Popen. Currently needs NOPASSWD sudo 
     access to interact with /dev/mem.
     """
 
-    # TODO: Figure out if a user can be given privs to access /dev/mem
-
+    sleep_time = 1
+    loops = 2
     timestamp = datetime.datetime.now()
 
-    cmd = ["/usr/bin/sudo", "/usr/local/bin/strogonanoff_sender.py", "-c", str(channel), "-b", str(button), action]
+    cmd = ["/usr/bin/sudo", "/usr/local/bin/strogonanoff_sender.py",
+           "-c", str(channel), "-b", str(button), action]
     try:
-        print "%s running channel=%s button=%s action=%s" % (timestamp, str(channel), str(button), action)
-        p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        print "%s sleeping 2" % timestamp
-        time.sleep(2)
-        print "%s running channel=%s button=%s action=%s" % (timestamp, str(channel), str(button), action)
-        p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        for i in range(loops):
+            print "%s running channel=%s button=%s action=%s" % (timestamp, str(channel), str(button), action)
+            print "%s running cmd=%s" % (timestamp, cmd)
+            p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            print "%s sleeping %d" % (timestamp, sleep_time)
+            time.sleep(sleep_time)
     except Exception as e:
         print("%s error:  %s" % (timestamp, e.strerror))
-        print("%s failed to run command: \"%s\"" % (timestamp, ' '.join(command)))
-    
+        print("%s failed to run command: \"%s\"" %
+              (timestamp, ' '.join(command)))
 
 
 def on_message(client, userdata, message):
@@ -64,6 +57,10 @@ def on_message(client, userdata, message):
     """
 
     timestamp = datetime.datetime.now()
+
+    if switches is None:
+        print "%s not configured yet, ignoring request" % (timestamp)
+        return False
 
     try:
         data = json.loads(message.payload)
@@ -82,9 +79,9 @@ def on_message(client, userdata, message):
             print "%s action (%s) is not valid" % (timestamp, data['action'])
             return False
 
-        action  = data['action']
+        action = data['action']
         channel = switches[data['switch']]['channel']
-        button  = switches[data['switch']]['button']
+        button = switches[data['switch']]['button']
 
         print "%s switch=%s channel=%s button=%s action=%s" % (timestamp, data['switch'], channel, button, action)
 
@@ -94,18 +91,37 @@ def on_message(client, userdata, message):
             print "%s action (%s) is not valid" % (timestamp, data['action'])
             return False
 
-        action  = data['action']
+        action = data['action']
         channel = data['channel']
-        button  = data['button']
+        button = data['button']
 
         print "%s channel=%s button=%s action=%s" % (timestamp, channel, button, action)
 
     else:
         print "%s failed to parse message=%s" % (timestamp, data)
-        
+
     runcmd(channel, button, action)
 
-    
+
+def on_config(client, userdata, message):
+
+    global switches
+
+    timestamp = datetime.datetime.now()
+    print "%s doing some config stuff...." % (timestamp)
+
+    try:
+        print "%s parsing json. message=%s" % (timestamp, message.payload)
+        data = json.loads(message.payload)
+    except:
+        print "%s failed to parse json. message=%s" % (timestamp, message.payload)
+
+        return False
+
+    switches = data['rooms']
+    print "%s configured correctly" % timestamp
+
+
 def on_connect(client, userdata, rc):
     timestamp = datetime.datetime.now()
     print "%s connected with result code=%s " % (timestamp, str(rc))
@@ -115,8 +131,13 @@ if __name__ == "__main__":
 
     timestamp = datetime.datetime.now()
 
+    # Bit hacky, making this global. But you know ;)
+    switches = None
+
     mqttc = paho.Client()
-    mqttc.on_message = on_message
+    mqttc.message_callback_add(topic_toggle, on_message)
+    mqttc.message_callback_add(topic_switches, on_config)
+
     mqttc.on_connect = on_connect
     if username is not None and password is not None:
         print "%s connected to MQTT with authentication" % timestamp
@@ -129,7 +150,8 @@ if __name__ == "__main__":
     except Exception as e:
         print "%s failed to connect: %s" % (timestamp, e)
 
-    mqttc.subscribe(topic)
+    print "%s subscribing to topic: %s" % (timestamp, topic_wildcard)
+    mqttc.subscribe(topic_wildcard)
 
     while mqttc.loop() == 0:
         pass
